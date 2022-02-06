c66bfb3166de2ca213aa49e0f33c2ff92f361797
Temam-Hashim <temamhashim3@gmail.com>
2022-02-06 16:05:18 +0300

    db

--- includes/db.php
+++ includes/db.php
@@ -1,14 +1,14 @@
 <?php
-        //local server
-    // $db['db_host'] = "localhost";
-    // $db['db_user'] = "root";
-    // $db['db_password'] = "";
-    // $db['db_name'] = "black_office";
+        // local server
+    $db['db_host'] = "localhost";
+    $db['db_user'] = "root";
+    $db['db_password'] = "";
+    $db['db_name'] = "black_office";
     // cloud server 
-    $db['db_host'] = "sql209.epizy.com";
-    $db['db_user'] = "epiz_28434227";
-    $db['db_password'] = "7VkVvVtgUZ";
-    $db['db_name'] = "epiz_28434227_black";
+    // $db['db_host'] = "sql209.epizy.com";
+    // $db['db_user'] = "epiz_28434227";
+    // $db['db_password'] = "7VkVvVtgUZ";
+    // $db['db_name'] = "epiz_28434227_black";
 
     foreach($db as $key => $value){
         define(strtoupper($key),$value);
