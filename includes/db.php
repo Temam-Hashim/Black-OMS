a3b155815b9f9f23e640780363aef5b38440eb29
Temam-Hashim <temamhashim3@gmail.com>
2022-02-06 15:49:43 +0300

    new update
    db and login

--- includes/db.php
+++ includes/db.php
@@ -2,8 +2,8 @@
         //local server
     $db['db_host'] = "localhost";
     $db['db_user'] = "root";
-    $db['db_password'] = " ";
-    $db['db_name'] = " black_office";
+    $db['db_password'] = "";
+    $db['db_name'] = "black_office";
     foreach($db as $key => $value){
         define(strtoupper($key),$value);
     }
