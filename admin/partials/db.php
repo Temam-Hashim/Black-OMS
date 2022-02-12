707adee0a29c6af58621a73f22481c0d916ca2ef
Temam-Hashim <temamhashim3@gmail.com>
2022-02-12 11:25:21 +0300

    db changed

--- admin/partials/db.php
+++ admin/partials/db.php
@@ -6,16 +6,16 @@
     // $db['db_name'] = "black_office";
 
          // cloud server
-        //  $db['db_host'] = "sql6.freemysqlhosting.net";
-        //  $db['db_user'] = "sql6470805";
-        //  $db['db_password'] = "Sf1BTIGTRC";
-        //  $db['db_name'] = "sql6470805";
+         $db['db_host'] = "sql6.freemysqlhosting.net";
+         $db['db_user'] = "sql6470805";
+         $db['db_password'] = "Sf1BTIGTRC";
+         $db['db_name'] = "sql6470805";
 
               // cloud server 2
-      $db['db_host'] = "sql209.byetcluster.com";
-      $db['db_user'] = "epiz_28434227";
-      $db['db_password'] = "7VkVvVtgUZ";
-      $db['db_name'] = "epiz_28434227_black_office";
+    //   $db['db_host'] = "sql209.epizy.com";
+    //   $db['db_user'] = "epiz_28434227";
+    //   $db['db_password'] = "7VkVvVtgUZ";
+    //   $db['db_name'] = "epiz_28434227_black_office";
 
     foreach($db as $key => $value){
         define(strtoupper($key),$value);
